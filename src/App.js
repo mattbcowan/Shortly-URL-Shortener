40f9540850dac85d292738eed4b1c14e9eb770f1
Matt Cowan <matt@mattbcowan.com>
2022-01-05 12:43:22 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import ShortenLinkCard from "./components/ShortenLinkCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Navigation>
+        <Navbar>
+          <ShortlyLogo src="/images/shortly-logo.svg" alt="Shortly Logo" />
+          <Hamburger>
+            <Bar />
+            <Bar />
+            <Bar />
+          </Hamburger>
+        </Navbar>
+      </Navigation>
+      <Main>
+        <HeaderImage src="/images/person-at-desk-mobile.svg" />
+        <Section>
+          <CallToAction>More than just shorter links</CallToAction>
+          <ActionText>
+            Build your brand’s recognition and get detailed insights on how your
+            links are performing.
+          </ActionText>
+          <GetStartedBtn>Get Started</GetStartedBtn>
+        </Section>
+        <ShortenLinkCard />
+      </Main>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  font-family: "Poppins", sans-serif;
+  padding: 1rem;
+  overflow: hidden;
+  margin: 0;
+`;
+
+const Main = styled.main``;
+
+const HeaderImage = styled.img`
+  width: 100vw;
+`;
+
+const Navigation = styled.div``;
+
+const Navbar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 0;
+`;
+
+const ShortlyLogo = styled.img``;
+
+const Hamburger = styled.div`
+  height: 21px;
+  width: 24px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  cursor: pointer;
+`;
+
+const Bar = styled.span`
+  width: 24px;
+  height: 3px;
+  background: #9e9aa8;
+  display: block;
+`;
+
+const CallToAction = styled.div`
+  font-size: 42px;
+  font-weight: 700;
+  text-align: center;
+  letter-spacing: -1.05px;
+  line-height: 48px;
+  padding: 2rem 0;
+  color: #34313d;
+`;
+
+const ActionText = styled.div`
+  font-size: 18px;
+  text-align: center;
+  letter-spacing: 0.12px;
+  line-height: 30px;
+  color: #9e9aa8;
+  padding-bottom: 2rem;
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const GetStartedBtn = styled.button`
+  color: #ffffff;
+  background: #2bd0d0;
+  font-size: 20px;
+  font-weight: 700;
+  padding: 1rem 3rem;
+  border: none;
+  border-radius: 50px;
+`;
---
+++ src/components/ShortenLinkCard.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+const ShortenLinkCard = () => {
+  return (
+    <Wrapper>
+      <LinkInput placeholder="Shorten a link here..." />
+      <ShortenButton>Shorten It!</ShortenButton>
+    </Wrapper>
+  );
+};
+
+export default ShortenLinkCard;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+  padding: 1.5rem;
+  color: #ffffff;
+  background: #3a3054;
+  border-radius: 1rem;
+  background-image: url("/images/shorten-card-blob.svg");
+  background-repeat: no-repeat;
+  background-position: top right;
+`;
+
+const LinkInput = styled.input`
+  display: flex;
+  font-size: 16px;
+  padding: 0.75rem;
+  margin-bottom: 1rem;
+  border: none;
+  outline: none;
+  border-radius: 0.25em;
+  width: 100%;
+
+  ::placeholder {
+    color: #34313d;
+  }
+`;
+
+const ShortenButton = styled.button`
+  color: #ffffff;
+  background: #2bd0d0;
+  font-size: 20px;
+  font-weight: 700;
+  padding: 1rem 3rem;
+  border: none;
+  width: 100%;
+  border-radius: 0.25em;
+`;
